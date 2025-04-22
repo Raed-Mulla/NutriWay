d040cb41de8188ede16294ae82a4cf90abf8b9c9
Raed <xraed20@gmail.com>
2025-04-22 17:39:34 +0300

    add all_specialists and specialist_detail and add urls

--- NutriWay/specialists/urls.py
+++ NutriWay/specialists/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 app_name = "specialists"
 
-urlpatterns = []
+urlpatterns = [
+    path('create/subscription/', views.create_subscription_plan, name='create_subscription_plan'),
+    path('create/general/', views.create_general_plan, name='create_general_plan'),
+    path('plans/subscription/', views.list_subscription_plan, name='list_subscription_plan'),
+    path('plans/general/', views.list_general_plan, name='list_general_plan'),
+    path('plans/my/', views.my_plans, name='my_plans'),
+    path('specialists/', views.all_specialists, name='all_specialists'),
+    path('specialist/<int:specialist_id>/', views.specialist_detail, name='specialist_detail')
+]
--- NutriWay/specialists/views.py
+++ NutriWay/specialists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .forms import SubscriptionPlanForm , GeneralPlanForm
 from .models import SubscriptionPlan , Generalplan
-from accounts.models import Specialist
+from accounts.models import Specialist , Certificate
 
 def create_subscription_plan(request:HttpRequest):
     if request.method == "POST":
@@ -45,3 +45,16 @@ def my_plans(request:HttpRequest):
     plans = SubscriptionPlan.objects.filter(Specialist=specialist)
     return render(request,'specialists/my_plans.html',{'plans' : plans})
 
+
+def all_specialists(request:HttpRequest):
+    specialists = Specialist.objects.all()
+    return render(request, 'public/specialists_list.html', {'specialists': specialists})
+
+def specialist_detail(request:HttpRequest , specialist_id):
+    try:
+        specialist = Specialist.objects.get(id=specialist_id)
+        certificate = Certificate.objects.filter(specialist=specialist)
+        plans = SubscriptionPlan.objects.filter(specialist=specialist)
+    except Specialist.DoesNotExist:
+        return redirect("core:home_view")
+    return render(request, 'public/specialist_detail.html', {'specialist': specialist,'certificate': certificate,'plans': plans})
