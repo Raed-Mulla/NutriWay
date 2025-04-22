c26214f658ce80cf9221235b76fb5c4071804c9f
Nawaf Alahmadi <nawaf.m.alahmadi@gmail.com>
2025-04-22 11:43:17 +0300

    Login and Register pages - Html Nav

--- NutriWay/accounts/urls.py
+++ NutriWay/accounts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "accounts"
 
-urlpatterns = []
+urlpatterns = [
+  path("login/", views.login_view, name="login_view"),
+  path("register/", views.register_view, name="register_view"),
+]
--- NutriWay/accounts/views.py
+++ NutriWay/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
+def login_view(request: HttpRequest):
+  return render(request, "accounts/login.html")
+
+def register_view(request: HttpRequest):
+  return render(request, "accounts/register.html")
