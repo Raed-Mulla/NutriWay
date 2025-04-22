8667f10cf29dc72ff56f491127af5c37088c65d0
Raed <xraed20@gmail.com>
2025-04-22 11:28:12 +0300

    models

--- NutriWay/accounts/models.py
+++ NutriWay/accounts/models.py
@@ -1,3 +1,69 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Person(models.Model):
+    class GenderChoices(models.TextChoices):
+        Male = "Male" , "Male"
+        Femele = "Femele" , "Femele"
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=8,choices= GenderChoices.choices)
+    
+    def __str__(self):
+        return self.user.username
+
+
+class PersonData(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE , related_name="Person")
+    height = models.FloatField()
+    weight = models.FloatField()
+    goal = models.TextField()
+    chronic_diseases = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"data for {self.person.user.username}"
+    
+class Specialist(models.Model):
+    class GenderChoices(models.TextChoices):
+        Male = "Male" , "Male"
+        Femele = "Femele" , "Femele"
+
+    class SpecialtyChoices(models.TextChoices):
+        clinical = 'clinical', 'Clinical Nutrition'
+        weight_managment = 'weight', 'Weight Management'
+        children = 'children', 'Pediatric Nutrition'
+        women = 'women', 'Womens Nutrition'
+        chronic = 'chronic', 'Chronic Conditions'
+        vegan = 'vegan', 'Vegan/Vegetarian Nutrition'
+        seniors = 'seniors', 'Geriatric Nutrition'
+        disorders = 'disorders', 'Eating Disorders'
+        preventive = 'preventive', 'Preventive Nutrition'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=8,choices= GenderChoices.choices)
+    birth_date = models.DateField()
+    specialization_certificate = models.FileField(upload_to='certificates/')
+    specialty = models.CharField(max_length=30, choices=SpecialtyChoices.choices)
+    image = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.user.username
+
+class Certificate(models.Model):
+    specialist = models.ForeignKey(Specialist ,on_delete=models.CASCADE , related_name="Certificates")
+    name = models.CharField(max_length=255)
+    image = models.FileField(upload_to='certificates/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.specialist.user.username}"
+
+class Director(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user.username
+
