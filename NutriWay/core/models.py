9e61bf4714c0c4ae7857eb90111144d227e56884
Raed <xraed20@gmail.com>
2025-04-23 14:33:04 +0300

    add reviews

--- NutriWay/core/models.py
+++ NutriWay/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from accounts.models import Person, Specialist
 # Create your models here.
+class Review(models.Model):
+    class RatingCohices(models.IntegerChoices):
+        one = 1 , "1"
+        two = 2 , "2"
+        three = 3 , "3"
+        four = 4 , "4"
+        five = 5 , "5"
+
+    person = models.ForeignKey(Person,on_delete=models.CASCADE,related_name="reviews")
+    specialist = models.ForeignKey(Specialist,on_delete=models.CASCADE,related_name="reviews")
+    comment = models.TextField()
+    rating = models.IntegerField(choices=RatingCohices)
+
+    def __str__(self):
+        return f"{self.person.user.username} → {self.specialist.user.username}"
--- NutriWay/core/urls.py
+++ NutriWay/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-  path("", views.home_view, name="home_view")
+  path("", views.home_view, name="home_view"),
+  path('specialist/<int:specialist_id>/add-review/', views.add_review, name='add_review')
 ]
