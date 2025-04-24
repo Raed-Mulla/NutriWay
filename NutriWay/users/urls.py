472d75aa5ba7e02a93da425bae4c7c110ee79e37
mohamad <mo.alharbi077@gmail.com>
2025-04-24 13:54:28 +0300

    setup useres views and urls

--- NutriWay/users/urls.py
+++ NutriWay/users/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = []
+urlpatterns = [
+    path('all_my_plans/<int:username>',views.all_my_plans,name='all_my_plans'),
+    path('subscription_detail/<int:subscription_id>/', views.subscription_detail , name='subscription_detail'),
+    path('subscription_detail/<int:subscription_id>/progress_report/', views.progress_report_view , name='progress_report_view'),
+    path('subscription_detail/<int:subscription_id>/check_meals/<int:day_number>',views.check_meals,name='check_meals'),
+    path('subscription_detail/view_progress/',views.subscription_progress_view,name='subscription_progress_view'),
+    path('subscription_detail/view_progress/<int:progress_report>',views.specialist_comment,'specialist_comment')
+]
--- NutriWay/users/views.py
+++ NutriWay/users/views.py
@@ -1,2 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from specialists.models import Specialist
+
+def all_my_plans(request:HttpRequest):  
+    pass
+
+def subscription_detail(reuqest:HttpRequest):
+    pass
+
+def progress_report_view(request:HttpRequest):
+    pass
+
+def check_meals(request:HttpRequest,day_number):
+    pass
+
+def specialist_commensubscription_detailt(request:HttpRequest):
+    pass
+
+def specialist_comment(request:HttpRequest):
+    pass
