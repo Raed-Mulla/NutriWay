970b6d04e5e880b90c4b532233fab67fcebeb93e
mohamad <mo.alharbi077@gmail.com>
2025-04-24 14:14:48 +0300

    fix 2

--- NutriWay/users/urls.py
+++ NutriWay/users/urls.py
@@ -5,9 +5,10 @@ app_name = "users"
 
 urlpatterns = [
     path('all_my_plans/<int:username>',views.all_my_plans,name='all_my_plans'),
+    path('subscription_to_plan/<int:subscriptionPlan_id>',views.subscription_to_plan,name='subscription_to_plan'),
     path('subscription_detail/<int:subscription_id>/', views.subscription_detail , name='subscription_detail'),
-    path('subscription_detail/<int:subscription_id>/progress_report/', views.progress_report_view , name='progress_report_view'),
-    path('subscription_detail/<int:subscription_id>/check_meals/<int:day_number>',views.check_meals,name='check_meals'),
-    path('subscription_detail/view_progress/',views.subscription_progress_view,name='subscription_progress_view'),
-    path('subscription_detail/view_progress/<int:progress_report>',views.specialist_comment,'specialist_comment')
+    path('subscription_detail/<int:subscription_id>/create/progress_report/', views.create_progress_report_view , name='create_progress_report_view'),
+    path('subscription_detail/<int:subscription_id>/check_meals/',views.check_meals,name='check_meals'),
+    path('subscription_detail/view_progress/',views.view_progress,name='view_progress'),
 ]
+
--- NutriWay/users/views.py
+++ NutriWay/users/views.py
@@ -2,20 +2,23 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from specialists.models import Specialist
 
+
+
+
 def all_my_plans(request:HttpRequest):  
+    pass
+
+def subscription_to_plan(request:HttpRequest):
     pass
 
 def subscription_detail(reuqest:HttpRequest):
     pass
 
-def progress_report_view(request:HttpRequest):
+def create_progress_report_view(request:HttpRequest):
     pass
 
 def check_meals(request:HttpRequest,day_number):
     pass
 
-def specialist_commensubscription_detailt(request:HttpRequest):
-    pass
-
-def specialist_comment(request:HttpRequest):
+def view_progress(request:HttpRequest):
     pass
