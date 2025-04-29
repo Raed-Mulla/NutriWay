54c0674112c4fa0573bf8311dc685e681534ada0
mohamad <mo.alharbi077@gmail.com>
2025-04-29 10:24:59 +0300

    finish accounts app

--- NutriWay/accounts/urls.py
+++ NutriWay/accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
   path("register/specialist/", views.specialist_register_view, name="specialist_register_view"),
   path('vertify/',views.vertify_view,name='vertify_view'),
   path('logout/',views.logout_view,name='logout_view'), 
-  path('profile/update/', views.update_profile_view, name='update_profile_view'),  
-  path('profile/<str:user_name>/', views.profile_view, name='profile_view'), 
+  path('profile/<str:user_name>/', views.profile_view, name='profile_view'),
+  path('profile/update/<str:user_name>', views.update_profile_view, name='update_profile_view'),  
+ 
 ]
