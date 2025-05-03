e58821a2e52b3fd00be65bd2f234115d5cea4d8d
Nawaf Alahmadi <nawaf.m.alahmadi@gmail.com>
2025-05-03 18:53:24 +0300

    Payment pages modiyfied

--- NutriWay/supports/views.py
+++ NutriWay/supports/views.py
@@ -53,10 +53,12 @@ def contact_messages(request : HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated:
         messages.error(request, "You must be logged in to access this page.", "alert-danger")
         return redirect('accounts:login_view')
-
-    messages_list = ContactUs.objects.all().order_by('-created_at')
-    
-    return render(request, 'supports/contact_messages.html', {'messages_list': messages_list})
+    if request.user.is_staff:
+        messages_list = ContactUs.objects.all().order_by('-created_at')
+        return render(request, 'supports/contact_messages.html', {'messages_list': messages_list})
+    else:
+        messages.error(request, "You are not authorized to access this page", "alert-danger")
+        return redirect('core:home_view')
 
 
 
