81a9ad45ddf5c44919bbc1a5aa7cf246118c09e9
Raed <xraed20@gmail.com>
2025-05-12 17:54:33 +0300

    edit

--- NutriWay/NutriWay/settings.py
+++ NutriWay/NutriWay/settings.py
@@ -96,7 +96,12 @@ DATABASES = {
         'HOST': os.environ["PGHOST"],
         'PORT': os.environ["PGPORT"],
     }
-} 
+} if not DEBUG else {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
